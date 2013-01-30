cf442e77aeba2d4d42cb688738733990c06eab9e
vadym <vadym@xxx.(none)>
2013-01-30 17:10:04 +0200

    update for demo page

--- lib/examples/Page.php
+++ lib/examples/Page.php
@@ -15,30 +15,64 @@ class examples_Page extends \Page {
 
         // we will load all maps by AJAX, so we need to add these lines to add static js to page
         $map=$this->add('x_gm\View_Map',array(
-            'sensor'=>'true',
             'libraries'=>array('drawing'),
         ));
         $map->addJsAndDestroy();
 
         $this->tt = $this->add('Tabs');
-        $this->tt->addTabUrl('./mapform','Form with map');
         $this->tt->addTabUrl('./map','Just Map');
+        $this->tt->addTabUrl('./mapform','Form with map');
         $this->tt->addTabUrl('./mapdraw','Map with drawing');
     }
     function page_map(){
         $map=$this->add('x_gm\View_Map',array(
             'sensor'=>'true',
+            'lat'=>'51.5081289',
+            'lng'=>'-0.128005',
         ));
-        //$map->addJs();
         $map->showMap();
     }
     function page_mapdraw(){
-        $map=$this->add('x_gm\View_Map',array(
-            'sensor'=>'true',
-            'libraries'=>array('drawing'),
+        $m = $this->add('Model_CountryD')->loadAny();
+        $f = $this->add('x_gm\Form_WithMap',array(
+            'form_config'=>array(
+                'draw'=>true,
+                'map_fields'=>array(
+                    'draw_field'=>'draw',
+                )
+            ),
+            'map_config'=>array(
+                'libraries'=>array('drawing'),
+                'sensor'=>true,
+                /*
+                 * mor info about polygon_options you can find here
+                 * https://developers.google.com/maps/documentation/javascript/reference#Polygon
+                 */
+                'polygon_options'=>array('single'=>false),
+                /*
+                 * more info about draw_options you can find here
+                 * https://developers.google.com/maps/documentation/javascript/reference
+                 */
+                'draw_options'=>"{
+                    drawingControlOptions: {
+                        position: google.maps.ControlPosition.TOP_LEFT,
+                        drawingModes: [
+                            google.maps.drawing.OverlayType.POLYGON
+                        ]
+                    },
+                    polygonOptions: {
+                        //fillColor: '#ffff00',
+                        //fillOpacity: 1,
+                        strokeWeight: 1,
+                        clickable: false,
+                        //zIndex: 1,
+                        editable: true
+                    }
+              }"
+            ),
         ));
-        //$map->addJs();
-        $map->showMap();
+        $f->setModel($m,array('draw'));
+        $f->addSubmit();
     }
     /*
      * This code will not work on your computer because you have no Model_Venue
@@ -47,9 +81,17 @@ class examples_Page extends \Page {
      */
     function page_mapform(){
         $form = $this->add('x_gm\Form_WithMap',array(
+                    'form_config'=>array(
+                        'location'=>true,
+                        'map_fields'=>array(
+                            'address_field'=>'address',
+                            'location_field'=>'f_location',
+                            'lat_field'=>'f_lat',
+                            'lng_field'=>'f_lon',
+                        )
+                    ),
                     'map_config' => array(
                         'sensor'=>'true',
-                        'libraries'=>array('drawing'),
                         'lat'=>'51.5081289',
                         'lng'=>'-0.128005',
                     ),

65dc02f148493fb67eeee619b8c4bab5bc9784fd
vadym <vadym@xxx.(none)>
2013-02-14 19:37:36 +0200

    no quick search and paginator

--- lib/View/Draw.php
+++ lib/View/Draw.php
@@ -77,11 +77,11 @@ class Grid_Draw extends \Grid {
     public $map;
     function init() {
         parent::init();
-        $this->addPaginator(8);
-        $this->add('QuickSearch',null,'quick_search')
-            ->removeClass('float-right')
-            ->useWith($this)
-            ->useFields(array('name'));
+//        $this->addPaginator(8);
+//        $this->add('QuickSearch',null,'quick_search')
+//            ->removeClass('float-right')
+//            ->useWith($this)
+//            ->useFields(array('name'));
     }
     function setModel($model, $actual_fields = undefined){
         parent::setModel($model, $actual_fields);
